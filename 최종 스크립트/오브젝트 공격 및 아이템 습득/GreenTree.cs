7fd53786bbbdc96daad1c72aa2777c83aa1422eb
EUNJU123 <84502789+EUNJU123@users.noreply.github.com>
2021-06-10 16:14:10 +0900

    칼로 과일나무를 베면 나무가 사라지고 나무가 없어진 자리에 아이템이 뜨고 인벤토리에 저장된다.

---
+++ 최종 스크립트/오브젝트 공격 및 아이템 습득/GreenTree.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GreenTree : MonoBehaviour
+{
+    public ParticleSystem tree; //나무가 없어질 때, 이펙트 정보를 저장하는 변수를 설정한다.
+    public GameObject tomato; //나무가 없어지면 설정한 쥬스가 나오도록 하는 변수 설정
+    public Transform PinkTreePost; //나무가 없어진 위치에 쥬스가 나오도록하는 변수 설정
+
+    int PinkTreeHp = 3; //나무의 체력변수 설정한다.
+
+    [SerializeField]
+    private Inventory theInventory;
+
+    void OnCollisionEnter(Collision col) //Collider간 발생한 충돌을 감지하는 함수 설정
+    {
+        if (col.gameObject.name == "w_sword_B") //만약 충돌하는 물체가 칼이라면
+        {
+            PinkTreeHp -= 1; //나무의 체력이 1 줄어든다.
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (PinkTreeHp == 0) //만약 나무의 체력이 0이라면
+        {
+            ParticleSystem fire = Instantiate(tree, transform.position, Quaternion.identity); //폭파 이펙트 함수 호출한다.
+            fire.Play();
+            theInventory.AcquireItem(GetComponent<ItemPickUp>().item); // ItemPickUp안에 item를 인벤토리에 저장한다.
+            Destroy(gameObject); //나무는 사라진다.
+            Instantiate(tomato, PinkTreePost.position, PinkTreePost.rotation); //나무가 없어진 자리에 쥬스가 뜨는 함수 코드
+        }
+    }
+}
