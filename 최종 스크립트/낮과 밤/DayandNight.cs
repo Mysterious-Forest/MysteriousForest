42bc28789897738b25640331f0a5c807f4f2377f
EUNJU123 <84502789+EUNJU123@users.noreply.github.com>
2021-06-10 15:06:05 +0900

    게임에서 시간의 흐름을 준다.

---
+++ 최종 스크립트/낮과 밤/DayandNight.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DayandNight : MonoBehaviour
+{
+    [SerializeField] private float isTime; //게임 시간과 현실 시간이 달라야 하기 때문에 변수 지정
+
+
+    // Start is called before the first frame update
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(Vector3.right, 0.1f * isTime * Time.deltaTime); //빛을 x축 방향으로 움직이도록 설정
+    }
+}
